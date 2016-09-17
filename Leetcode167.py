d2bc20775dce4d037fe0cfc518e04c67be6ff8a8
QuanZhou <joshzhou16@gmail.com>
2016-09-17 17:38:44 +0200

    two pointer

---
+++ Leetcode167.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i, j = 0, len(numbers) - 1
+        while numbers[i] + numbers[j] != target:
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+
+        return [i+1, j+1]
+
+print(Solution().twoSum(numbers=[0,0,3,4,4], target=0))
